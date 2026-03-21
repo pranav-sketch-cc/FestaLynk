49e136c6cfa26f665581ed907997bdbd19d38365
pranav-sketch-cc <pranavvarshan2007@gmail.com>
2026-03-21 22:48:22 +0530

    initial commit splash-screen added

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatelessWidget{
+  final bool _visible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF4F5F7),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(seconds: 3),
+      child:
+      Image.asset('images/splash_logo.png'),
+      ),
+    ),
+  );
+
+  }
+
+}
