1d6bcf8149e817657121605a258e7d4c74cb915d
pranav-sketch-cc <pranavvarshan2007@gmail.com>
2026-03-22 11:13:49 +0530

    splash try

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'login.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
-class SplashScreen extends StatelessWidget{
-  final bool _visible = true;
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 8), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF4F5F7),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _visible ? 1.0 : 0.0,
-          duration: Duration(seconds: 3),
-      child:
-      Image.asset('images/splash_logo.png'),
-      ),
-    ),
-  );
+      backgroundColor: Color(value),
+      body:
+          Center(
+            child: Image.asset(
+              'images/splash_logo.png',
+              width: 150,
+            ),
+          ),
+      );
   }
 }
