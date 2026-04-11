398bea3584d16d2b6f92681abe35c181bb4c2076
pranav-sketch-cc <pranavvarshan2007@gmail.com>
2026-04-11 22:46:09 +0530

    create page ui complete

--- lib/create.dart
+++ lib/create.dart
@@ -90,12 +90,115 @@ class CreatePage extends StatelessWidget {
 
                   const SizedBox(height: 10),
 
-                  Image.asset("images/uploadimg.png",),
+                  Stack(
+                    alignment: Alignment(0.07,0.38),
+                    children: [
+                       Image.asset("images/uploadimg.png",),
+
+                      SizedBox(
+                        width: 160,
+                        height: 40,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF1D61E7),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(3),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "Upload",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                      ),
+
+
+                   ],
+                  ),
 
                   const SizedBox(height: 20),
 
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                                "Start date",
+                              style: TextStyle(
+                                color: Color(0xFF6F7994),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              )
+                            ),
+
+                            const SizedBox(height: 8),
+
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: "Start date",
+                                hintStyle: TextStyle(
+                                    color: Colors.grey
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(width: 10),
+
+
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                                "End date",
+                              style: TextStyle(
+                                color: Color(0xFF6F7994),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              )
+                            ),
+
+                            const SizedBox(height: 8),
+
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: "End date",
+                                hintStyle: TextStyle(
+                                    color: Colors.grey
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 20),
+
                   const Text(
-                    "Enter Start Date",
+                    "Enter Location",
                     style: TextStyle(
                       color: Color(0xFF6F7994),
                       fontSize: 18,
@@ -107,7 +210,185 @@ class CreatePage extends StatelessWidget {
 
                   TextField(
                     decoration: InputDecoration(
-                      hintText: "Start Date",
+                      hintText: "Location",
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  const Text(
+                    "Enter Registration URL",
+                    style: TextStyle(
+                      color: Color(0xFF6F7994),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "URL",
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  const Text(
+                    "Enter Mode of Event",
+                    style: TextStyle(
+                      color: Color(0xFF6F7994),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Mode of Event",
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  const Text(
+                    "Enter Registration Amount",
+                    style: TextStyle(
+                      color: Color(0xFF6F7994),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Registration Amount",
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  const Text(
+                    "Enter Event Type",
+                    style: TextStyle(
+                      color: Color(0xFF6F7994),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Event Type",
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  const Text(
+                    "Enter Event Details",
+                    style: TextStyle(
+                      color: Color(0xFF6F7994),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  TextField(
+                    maxLength: 500,
+                    maxLines: 5,
+                    minLines: 1,
+                    keyboardType: TextInputType.multiline,
+                    decoration: InputDecoration(
+                      hintText: "Event Details",
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.only(
+                        bottom: 100,
+                        top: 10,
+                        left: 10,
+                        right: 10,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  const Text(
+                    "Enter Registration Deadline",
+                    style: TextStyle(
+                      color: Color(0xFF6F7994),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Registration Deadline",
                       hintStyle: const TextStyle(
                         color: Colors.grey,
                       ),
@@ -121,11 +402,8 @@ class CreatePage extends StatelessWidget {
                   ),
 
 
-
-
-
-
                   const SizedBox(height: 20),
+
                   SizedBox(
                     width: double.infinity,
                     height: 50,
@@ -138,7 +416,7 @@ class CreatePage extends StatelessWidget {
                       ),
                       onPressed: () {},
                       child: const Text(
-                        "Uploaded",
+                        "Upload",
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
